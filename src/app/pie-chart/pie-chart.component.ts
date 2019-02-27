3c6a277c850667dacc322d177075e8fc8ef0f9e9
balakrishnanUIDeveloper <krish.18.2009@gmail.com>
2019-02-27 11:51:54 +0530

    Update pie-chart.component.ts

--- src/app/pie-chart/pie-chart.component.ts
+++ src/app/pie-chart/pie-chart.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pie-chart',
-  templateUrl: './pie-chart.component.html',
-  styleUrls: ['./pie-chart.component.scss']
+  templateUrl: './pie-chart.component.html'  
 })
 export class PieChartComponent implements OnInit {
   @Input() chartOptions: any;
